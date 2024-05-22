554d92b47c7b00b95a62d342cec22afa00c5defc
Kacper0510 <wkacper@spoko.pl>
2024-05-22 23:55:17 +0200

    Add reading images and saving to PDF

--- calendar-generator.py
+++ calendar-generator.py
@@ -1,27 +1,62 @@
 from sys import argv
+from os import path, listdir
 from datetime import datetime, timedelta
 import json
+from PIL import Image, UnidentifiedImageError
+from itertools import cycle, islice
 
 # Parameters to tweak
 
 NAME_DAYS_FILE: str = "polish_name_days.json"
+IMAGES_DIRECTORY: str = "images"
+
+AUTHOR_NAME: str = "Kacper Wojciuch"
+PDF_DPI: int = 75  # DPI 75, so the default image should be 842x1191 for A3
 
 # Script code
 
-NAME_DAYS: dict[tuple[int, int], list[str]]  # (day, month) -> [name1, name2, ...]
 
-
-def read_name_days() -> None:
-    """Reads name days from a JSON file into a global variable"""
-    global NAME_DAYS
+def read_name_days() -> dict[tuple[int, int], list[str]]:
+    """
+    Reads name days from a JSON file into a dict with the following structure:
+    (day, month) -> [name1, name2, ...]
+    """
     with open(NAME_DAYS_FILE, "r", encoding="UTF-8") as file:
         names: dict[str, list[str]] = json.load(file)
-        NAME_DAYS = {tuple(map(int, key.split("."))): value for key, value in names.items()}
+        return {tuple(map(int, key.split("."))): value for key, value in names.items()}  # type: ignore
+
+
+def read_embedded_images() -> list[Image.Image]:
+    """
+    Reads images to be embedded into the calendar from the selected directory
+    and produces a list of exactly 12 elements, one for each month.
+    """
+    image_names = [file for f in listdir(IMAGES_DIRECTORY) if path.isfile(file := path.join(IMAGES_DIRECTORY, f))]
+    images: list[Image.Image] = []
+    for image in image_names:
+        try:
+            images.append(Image.open(image).convert("RGB"))
+        except UnidentifiedImageError:
+            print(f"Could not open image: {image}")
+    if len(images) == 0:
+        print("No images found in the selected directory!")
+        raise ValueError
+    return list(islice(cycle(images), 12))
 
 
 def main(year: int) -> None:
     """Main function"""
-    read_name_days()
+    name_days = read_name_days()
+    embedded_images = read_embedded_images()
+    embedded_images[0].save(
+        f"calendar_{year}.pdf",
+        save_all=True,
+        append_images=embedded_images[1:],
+        author=AUTHOR_NAME,
+        producer="Kacper0510/calendar-generator",
+        title=f"{year} - {AUTHOR_NAME}",
+        resolution=PDF_DPI,
+    )
 
 
 if __name__ == "__main__":
